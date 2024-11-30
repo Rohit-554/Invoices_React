dc7086c4f0e44e0e26e063fb4413aa1f04dc3fe4
Jadu <48874687+Rohit-554@users.noreply.github.com>
2024-11-30 20:00:40 +0530

    font change

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Quicksand } from 'next/font/google';
  
-export const inter = Quicksand({ weight: [ "400"] });
-export const lusitana = Quicksand({ weight: [ "400"] });
+export const inter = Quicksand({ weight: [ "400"],subsets: ['latin'] });
+export const lusitana = Quicksand({ weight: [ "400"], subsets: ['latin'] });
